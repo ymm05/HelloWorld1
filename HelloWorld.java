3914da05d2927a943b48c94cfdf2862d2852f4e5
Yasin McCaskill <yasin.mccaskill@mrjaffesclass.com>
2022-09-12 14:24:24 -0700

    made new file

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (Yasin McCaskill)
+ * @version (9-12-22)
+ */
+public class HelloWorld
+{
+    public class HelloWorld1 
+{
+    public void main(String[] args) 
+    {
+        System.out.println("Hello World!") ;
+    }
+}
+}
