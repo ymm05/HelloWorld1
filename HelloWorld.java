6eb95e68fa415f562e029488e70cb9a3cf8cb7ce
Yasin McCaskill <yasin.mccaskill@mrjaffesclass.com>
2022-09-13 14:13:48 -0700

    Commit message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,7 @@
-
-/**
- * Write a description of class HelloWorld here.
- *
- * @author (Yasin McCaskill)
- * @version (9-12-22)
- */
-public class HelloWorld
+public class HelloWorld 
 {
-    public class HelloWorld1 
-{
-    public void main(String[] args) 
+    public static void main(String[] args) 
     {
-        System.out.println("Hello World!") ;
+        System.out.println("Hello World!");
     }
-}
 }
